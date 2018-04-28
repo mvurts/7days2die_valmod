f7f903c97be0e0fca23d8dfdb715eb747ecaad6b
S-Kerrigan(H) <Kerrigan.S.J@Gmail.com>
2018-04-28 17:33:18 +0700

    Improved XMLEngine - added result of parsing to JSON-format

--- server/src/main/java/com/zombie/infrastructure/XMLEngine.java
+++ server/src/main/java/com/zombie/infrastructure/XMLEngine.java
@@ -6,11 +6,19 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONValue;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class XMLEngine {
 
@@ -20,7 +28,9 @@ public class XMLEngine {
 		this.xml = xml;
 	}
 
-	public void parse() throws ParserConfigurationException, IOException, SAXException {
+	public String parse() throws ParserConfigurationException, IOException, SAXException {
+
+		List result = new ArrayList();
 
 		File file                       = new File(this.xml);
 		DocumentBuilderFactory factory  = DocumentBuilderFactory.newInstance();
@@ -32,7 +42,9 @@ public class XMLEngine {
 		NodeList recipes = xmlDoc.getElementsByTagName("recipe");
 
 		for (int i = 0; i < recipes.getLength(); i++) {
-			Node node = recipes.item(i);
+			Map<String, Object> map     = new HashMap<>();
+			List _result                = new ArrayList();
+			Node node                   = recipes.item(i);
 
 			System.out.println("\n");
 
@@ -41,18 +53,37 @@ public class XMLEngine {
 				Element element     = (Element) node;
 				NodeList components = element.getElementsByTagName("ingredient");
 
+				map.put("name", element.getAttribute("name"));
+				map.put("count", element.getAttribute("count"));
+				map.put("tollArea", element.getAttribute("craft_area"));
+
 				System.out.println("name: " + element.getAttribute("name"));
 				System.out.println("count: " + element.getAttribute("count"));
 				System.out.println("tollArea: " + element.getAttribute("craft_area"));
 				for (int y = 0; y < components.getLength(); y++) {
 					Node _node = components.item(y);
 					if (_node.getNodeType() == Node.ELEMENT_NODE) {
-						Element _element     = (Element) _node;
+						Map<String, Object> _map    = new HashMap<>();
+						Element _element            = (Element) _node;
+
+						_map.put("name", _element.getAttribute("name"));
+						_map.put("count", _element.getAttribute("count"));
+						_result.add(_map);
+
 						System.out.println("\t"+_element.getAttribute("name") + " -> " + _element.getAttribute("count"));
 					}
 				}
+
+				map.put("ingredients",_result);
+				result.add(map);
 			}
 		}
+
+		String jsonText = JSONValue.toJSONString(result);
+		System.out.println("JSON result...");
+		System.out.println(jsonText);
+
+		return jsonText;
 	}
 
 }
